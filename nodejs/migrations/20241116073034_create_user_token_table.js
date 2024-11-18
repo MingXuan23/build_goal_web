263f34d2c3d9c561a5da0a16559d1e20afa06778
haz1qq <mhaziqfadhlan03@gmail.com>
2024-11-18 20:41:34 +0800

    updated user_token script

--- nodejs/migrations/20241116073034_create_user_token_table.js
+++ nodejs/migrations/20241116073034_create_user_token_table.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('user_token', function (table) {
         table.bigIncrements('id').primary(); // Primary key, auto-incrementing ID
         table.bigInteger('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-        table.datetime('created_at').nullable(); // Created at, optional
+        table.timestamps(true, true); // Created at & Updated at timestamps
         table.datetime('expired_at').nullable(); // Expired at, optional
         table.bigInteger('app_id').unsigned().references('id').inTable('application').onDelete('CASCADE');
         table.string('desc').nullable(); // Description, optional
