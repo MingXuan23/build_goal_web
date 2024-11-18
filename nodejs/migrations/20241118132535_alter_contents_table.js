d6cfaea2f00bda6dcfd1e2afb476094f46738dc5
haz1qq <mhaziqfadhlan03@gmail.com>
2024-11-18 21:31:46 +0800

    alter content (reason_phrase) table

---
+++ nodejs/migrations/20241118132535_alter_contents_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('contents', function (table) {
+        table.string('reason_phrase').nullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('contents', function (table) {
+      table.dropColumn('reason_phrase'); // Remove the column if rolled back
+    });
+};
