24ac5bc4eb35231898fd1ba033fec4a1f7e0030e
haz1qq <mhaziqfadhlan03@gmail.com>
2024-12-14 23:08:40 +0800

    alter user and gpt_table

---
+++ nodejs/migrations/20241214150259_alter_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('users', function(table) {
+        table.boolean('is_gpt').defaultTo('false').alter();
+        table.boolean('gpt_status').defaultTo('false').alter();
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.table('users', function(table) {
+        table.boolean('is_gpt').defaultTo('true');
+        table.boolean('gpt_status').defaultTo('true');//rollback
+    });
+  };
+  
