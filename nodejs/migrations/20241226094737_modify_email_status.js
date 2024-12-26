4011f4f2aa9f4fc1694a59488db91dd54f7073aa
haz1qq <mhaziqfadhlan03@gmail.com>
2024-12-26 17:52:20 +0800

    Create 20241226094737_modify_email_status.js

---
+++ nodejs/migrations/20241226094737_modify_email_status.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('email_status', function(table) {
+        table.timestamp('created_at').defaultTo(knex.fn.now()).nullable();
+        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')).nullable();// Created at & Updated at timestamps
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('email_status', function(table) {
+        table.dropColumn('created_at');
+        table.dropColumn('updated_at');
+
+    });
+};
