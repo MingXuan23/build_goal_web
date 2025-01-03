9aa57e058387ece75dcf7171b6da1cb8c2b9daf4
Khairul Adzhar <adzharkhai01@gmail.com>
2025-01-04 04:56:57 +0800

    Mail Server Setup

--- laravel/resources/views/admin/layouts/sidebar.blade.php
+++ laravel/resources/views/admin/layouts/sidebar.blade.php
@@ -148,6 +148,18 @@
                 </li>
                 <li class="slide has-sub">
                     <a href="javascript:void(0);" class="side-menu__item ">
+                        <i class='bx bx-voicemail side-menu__icon'></i>
+                        <span class="side-menu__label ">Mail Server</span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+                    <ul class="slide-menu child1">
+                        <li class="slide">
+                            <a href="https://mail.xbug.online:8090/snappymail/index.php" target="_blank" class="side-menu__item">Mail Server Email</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="slide has-sub">
+                    <a href="javascript:void(0);" class="side-menu__item ">
                         <i class='bx bxs-cog side-menu__icon'></i>
                         <span class="side-menu__label ">Setting</span>
                         <i class="fe fe-chevron-right side-menu__angle"></i>
